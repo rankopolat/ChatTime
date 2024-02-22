c2634ac9de5dbd0e374eb203a449969b88b1d9a2
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-02-22 22:10:59 +1100

    simple fixes

--- src/Todo.js
+++ src/Todo.js
@@ -3,18 +3,19 @@ import React, { useState, useEffect } from 'react';
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from './firebase'; // Import the initialized Firebase app
 
-
-const Todo = () => {
+function Todo(){
+    
     const [todo, setTodo] = useState("");
     const [todos, setTodos] = useState([]);
 
     const addTodo = async (e) => {
         e.preventDefault();
+        
         try {
             const docRef = await addDoc(collection(db, "todos"), {
                 todo: todo,
             });
-            console.log("Document written with ID: ", docRef.id);
+
             setTodos([...todos, { id: docRef.id, todo: todo }]);
             setTodo(""); // Clear input field after adding todo
         } catch (error) {
@@ -22,19 +23,22 @@ const Todo = () => {
         }
     };
 
-    const fetchTodos = async () => {
+    async function fetchTodos(){
         try {
             const querySnapshot = await getDocs(collection(db, "todos"));
-            const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+            const newData = querySnapshot.docs.map((doc) => ({...doc.data(), id: doc.id }));
+
             setTodos(newData);
         } catch (error) {
             console.error("Error fetching todos: ", error);
         }
-    };
+    }
 
     useEffect(() => {
         fetchTodos();
-    }, []);
+    }, []); 
+
 
     return (
         <div>
@@ -47,13 +51,13 @@ const Todo = () => {
                 />
                 <button type="submit">Add</button>
             </form>
-            <ul>
-                {todos.map((todoItem) => (
-                    <li key={todoItem.id}>{todoItem.todo}</li>
+            <ol>
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.todo}</li>
                 ))}
-            </ul>
+            </ol>
         </div>
     );
-};
+}
 
 export default Todo;
